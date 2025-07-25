9c7ad10820ab84e11ed67bdd9157c9aa393c791d
Roger Savage <rogersavage@Rogers-MacBook-Pro.local>
2025-07-25 16:09:54 +0100

    Update config for GitHub Pages

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,15 +15,15 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://maven-knowledge.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/maven-knowledge-docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'Maven Knowledge', // Usually your GitHub org/user name.
-  projectName: 'MK API', // Usually your repo name.
+  organizationName: 'maven-knowledge', // Usually your GitHub org/user name.
+  projectName: 'maven-knowledge-docs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
